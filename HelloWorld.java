dd2e8dc4d7bc16e047c0fadc2d97e4b8f76c9b02
AndreaSierra1561 <sierra.a005008@stu.sanjac.edu>
2025-06-08 03:53:06 +0000

    Note to self this is the commit file

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld // this line is to create and name the class
+{
+    /**
+     * This is the main method of my program  
+     * @param - array of String args from the console // this line is to explain the method
+     */
+    public static void main(String [] args) //this line allows the program to run an array of string values
+    {
+        String a = "Andrea Sierra"; //this line give the value Andrea Sierra to string a 
+        String b = "Pasta"; //this line gives the value Pasta to string b 
+        String c = "Adele"; //this line gives the value Adele to string c 
+        String d = "Washing dishes"; //this line gives the value washing dishes to string d
+        String e = "Watching The Last of Us"; //this line give the value whatching t... to string e
+        System.out.println(a); //this line allows the program to print the argument a
+        System.out.println(b); //this line allows the program to print the argument b
+        System.out.println(c); //this line allows the program to print the argument c
+        System.out.println(d); //this line allows the program to print the argument d
+        System.out.println(e); //this line allows the program to print the argument e
+    }
+    /**
+     * We use main() because this gives the program the starting point 
+     * to execute the commands 
+     */
+    /**
+     * We use System.out.println() to print the massages to the console 
+     */
+    /**
+     * The purpose of comments are to describe to yourself and other what your codes 
+     * mean. It makes the codes easier to understand
+     */
+}
