ce1aa85e7c3ff14a4bd31be2d5d530c88a4dad3f
AndreaSierra1561 <sierra.a005008@stu.sanjac.edu>
2025-06-08 00:16:39 +0000

    note to self about these changes

---
+++ VariablesExample.java
@@ -0,0 +1,16 @@
+public class VariablesExample
+{
+    /**
+     * this is the main method of my program 
+     * @param - array of String args from the console
+     */
+    public static void main(String [] args)
+    {
+        String a = "Hello";
+        int b = 3;
+        int c = 5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
